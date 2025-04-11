5476417cea9cf0f61313c357f0f0ee4ddd037095
Technology Rooms <technologyrooms@colsanjose.edu.co>
2025-04-11 13:05:11 -0500

    1

---
+++ src/buttons/Function.java
@@ -0,0 +1,33 @@
+package buttons;
+
+import javax.swing.Icon;
+import javax.swing.JButton;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author technologyrooms
+ */
+public class Function {
+
+    public static JButton boton1 = null;
+    public static Icon iconG = null;
+
+    public static void FunctionOfButtons(JButton boton2) {
+        if(boton2!=boton1){
+        if (iconG != null) {
+            boton2.setIcon(iconG);
+            iconG = null;
+            boton1.setIcon(null);
+        } else {
+            if (boton2.getIcon() != null) {
+                iconG = boton2.getIcon();
+                boton1 = boton2;
+            }
+        }
+    }
+    }
+}
---
+++ src/buttons/Taken.java
@@ -0,0 +1,13 @@
+package buttons;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author technologyrooms
+ */
+public class Taken {
+     public static Boolean t = false;
+}
